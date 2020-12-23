56b0b45b2766b4816410cf2f2552dfd60e6ad94e
hogashi <hogextend@gmail.com>
2020-12-23 20:15:36 +0900

    render options in react

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,16 +1,23 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 type HostnameSet = {
   [hostname: string]: true;
 };
 
+const getHostnameSet = (): HostnameSet => JSON.parse(localStorage.getItem(HOSTNAME_SET) || '{}');
+
 const App: React.FC = () => {
+  const [hostnameSet, setHostnameSet] = useState(getHostnameSet());
+  const hostnames = Object.keys(hostnameSet);
+  const hostNamesOptions = hostnames.map(hostname => <option key={hostname} value={hostname}>{hostname}</option>);
+
   return (
     <div>
       <div>
         <select id="hostname-selector">
           <option id="default-option" value="">select existing hostname</option>
+          { hostNamesOptions }
         </select>
         <button id="load-button">load</button>
       </div>
@@ -42,7 +49,6 @@ const saveButton = document.querySelector<HTMLButtonElement>('#save-button')!;
 let loadButtonTimer: ReturnType<typeof setTimeout>;
 let saveButtonTimer: ReturnType<typeof setTimeout>;
 
-const getHostnameSet = (): HostnameSet => JSON.parse(localStorage.getItem(HOSTNAME_SET) || '{}');
 const addHostname = (hostname: string, set: HostnameSet): void => {
   set[hostname] = true;
   localStorage.setItem(HOSTNAME_SET, JSON.stringify(set));
@@ -52,15 +58,6 @@ const addOrUpdateStyleAndHostname = (set: HostnameSet, hostname: string, style: 
   addHostname(hostname, set);
   localStorage.setItem(hostname, style);
 };
-
-// TODO: in React
-const hostnameSet = getHostnameSet();
-Object.keys(hostnameSet).forEach(hostname => {
-  const option = document.createElement('option');
-  option.value = hostname;
-  option.innerText = hostname;
-  hostnameSelector.appendChild(option);
-});
 
 const rememberLasSelectedHostname = (hostname: string) => localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
 
