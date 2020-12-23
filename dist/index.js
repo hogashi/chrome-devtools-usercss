f6a7bc232f79e0981ca05c34b949b12de5969396
hogashi <hogextend@gmail.com>
2020-12-23 19:44:34 +0900

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,22 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ 607:
-/***/ ((__unused_webpack_module, exports) => {
-
-var __webpack_unused_export__;
-
-__webpack_unused_export__ = ({ value: true });
 var message = {
-    hostname: location.hostname,
+  hostname: location.hostname
 };
-var responseCallback = function (response) {
-    var _a;
-    console.log(response);
-    var style = response.style;
-    if (style === null) {
-        return;
-    }
-    (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.insertAdjacentHTML('beforeend', "<style data-usercss>" + style + "</style>");
+
+var responseCallback = function responseCallback(response) {
+  var _document$querySelect;
+
+  console.log(response);
+  var style = response.style;
+
+  if (style === null) {
+    return;
+  }
+
+  (_document$querySelect = document.querySelector('body')) === null || _document$querySelect === void 0 ? void 0 : _document$querySelect.insertAdjacentHTML('beforeend', "<style data-usercss>".concat(style, "</style>"));
 };
+
 console.log('loaded');
 window.chrome.runtime.sendMessage(message, responseCallback);
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	// startup
-/******/ 	// Load entry module
-/******/ 	__webpack_require__(607);
-/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
--- dist/popup.js
+++ dist/popup.js
@@ -1,6 +1,4 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-
 var HOSTNAME_SET = 'hostnameSet';
 var LAST_SELECTED_HOST_NAME = 'lastSelectedHostname';
 var hostnameInput = document.querySelector('#hostname-input');
@@ -11,65 +9,84 @@ var textarea = document.querySelector('#textarea');
 var saveButton = document.querySelector('#save-button');
 var loadButtonTimer;
 var saveButtonTimer;
-var getHostnameSet = function () { return JSON.parse(localStorage.getItem(HOSTNAME_SET) || '{}'); };
-var addHostname = function (hostname, set) {
-    set[hostname] = true;
-    localStorage.setItem(HOSTNAME_SET, JSON.stringify(set));
+
+var getHostnameSet = function getHostnameSet() {
+  return JSON.parse(localStorage.getItem(HOSTNAME_SET) || '{}');
 };
-var getStyleByHostname = function (hostname) { return localStorage.getItem(hostname) || ''; };
-var addOrUpdateStyleAndHostname = function (set, hostname, style) {
-    addHostname(hostname, set);
-    localStorage.setItem(hostname, style);
+
+var addHostname = function addHostname(hostname, set) {
+  set[hostname] = true;
+  localStorage.setItem(HOSTNAME_SET, JSON.stringify(set));
 };
-// TODO: in React
+
+var getStyleByHostname = function getStyleByHostname(hostname) {
+  return localStorage.getItem(hostname) || '';
+};
+
+var addOrUpdateStyleAndHostname = function addOrUpdateStyleAndHostname(set, hostname, style) {
+  addHostname(hostname, set);
+  localStorage.setItem(hostname, style);
+}; // TODO: in React
+
+
 var hostnameSet = getHostnameSet();
 Object.keys(hostnameSet).forEach(function (hostname) {
-    var option = document.createElement('option');
-    option.value = hostname;
-    option.innerText = hostname;
-    hostnameSelector.appendChild(option);
+  var option = document.createElement('option');
+  option.value = hostname;
+  option.innerText = hostname;
+  hostnameSelector.appendChild(option);
 });
-var rememberLasSelectedHostname = function (hostname) { return localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname); };
-// TODO: do onChangeSelector
-var onLoadButtonClick = function () {
-    var selectedOption = hostnameSelector.selectedOptions[0];
-    var hostname = selectedOption.value;
-    if (hostname.length === 0) {
-        loadButton.innerText = 'select hostname';
-        clearTimeout(loadButtonTimer);
-        loadButtonTimer = setTimeout(function () {
-            loadButton.innerText = 'load';
-        }, 1500);
-        return;
-    }
-    hostnameInput.value = hostname;
-    var style = localStorage.getItem(hostname) || '';
-    textarea.value = style;
-    localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
+
+var rememberLasSelectedHostname = function rememberLasSelectedHostname(hostname) {
+  return localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
+}; // TODO: do onChangeSelector
+
+
+var onLoadButtonClick = function onLoadButtonClick() {
+  var selectedOption = hostnameSelector.selectedOptions[0];
+  var hostname = selectedOption.value;
+
+  if (hostname.length === 0) {
+    loadButton.innerText = 'select hostname';
+    clearTimeout(loadButtonTimer);
+    loadButtonTimer = setTimeout(function () {
+      loadButton.innerText = 'load';
+    }, 1500);
+    return;
+  }
+
+  hostnameInput.value = hostname;
+  var style = localStorage.getItem(hostname) || '';
+  textarea.value = style;
+  localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
 };
-var onSaveButtonClick = function () {
-    var hostname = hostnameInput.value;
-    if (hostname.length === 0) {
-        saveButton.innerText = 'input hostname';
-        clearTimeout(saveButtonTimer);
-        saveButtonTimer = setTimeout(function () {
-            saveButton.innerText = 'save';
-        }, 1500);
-        return;
-    }
-    hostnameSet[hostname] = true;
-    localStorage.setItem(HOSTNAME_SET, JSON.stringify(hostnameSet));
-    var newStyle = textarea.value;
-    localStorage.setItem(hostname, newStyle);
-    localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
+
+var onSaveButtonClick = function onSaveButtonClick() {
+  var hostname = hostnameInput.value;
+
+  if (hostname.length === 0) {
+    saveButton.innerText = 'input hostname';
+    clearTimeout(saveButtonTimer);
+    saveButtonTimer = setTimeout(function () {
+      saveButton.innerText = 'save';
+    }, 1500);
+    return;
+  }
+
+  hostnameSet[hostname] = true;
+  localStorage.setItem(HOSTNAME_SET, JSON.stringify(hostnameSet));
+  var newStyle = textarea.value;
+  localStorage.setItem(hostname, newStyle);
+  localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
 };
+
 loadButton.addEventListener('click', onLoadButtonClick);
 saveButton.addEventListener('click', onSaveButtonClick);
 var lastSelectedHostname = localStorage.getItem(LAST_SELECTED_HOST_NAME);
-if (lastSelectedHostname && hostnameSet[lastSelectedHostname]) {
-    document.querySelector("option[value=\"" + lastSelectedHostname + "\"]").selected = true;
-    onLoadButtonClick();
-}
 
+if (lastSelectedHostname && hostnameSet[lastSelectedHostname]) {
+  document.querySelector("option[value=\"".concat(lastSelectedHostname, "\"]")).selected = true;
+  onLoadButtonClick();
+}
 /******/ })()
 ;
