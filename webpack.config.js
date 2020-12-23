eba4f32d0e4f8fd5254e2134cea735b331600170
hogashi <hogextend@gmail.com>
2020-12-23 19:43:32 +0900

    add @babel/preset-typescript

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,12 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: [
-          { loader: 'ts-loader' },
           {
             loader: 'babel-loader',
             options: {
               presets: [
                 '@babel/preset-env',
+                '@babel/preset-typescript',
                 [
                   '@babel/preset-react',
                     // for new jsx transform in react 17 https://babeljs.io/blog/2020/03/16/7.9.0#a-new-jsx-transform-11154httpsgithubcombabelbabelpull11154
