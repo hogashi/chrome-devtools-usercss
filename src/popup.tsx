fcd343c34e0c1d6d3ee520d124c4f97686866d2d
hogashi <hogextend@gmail.com>
2020-12-30 20:09:30 +0900

    fix order

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,11 +5,13 @@ type HostnameSet = {
   [hostname: string]: true;
 };
 
+const HOSTNAME_SET = 'hostnameSet';
+const LAST_SELECTED_HOST_NAME = 'lastSelectedHostname';
+
 const PLACEHOLDER = `body {
   color: magenta;
 }`;
-const HOSTNAME_SET = 'hostnameSet';
-const LAST_SELECTED_HOST_NAME = 'lastSelectedHostname';
+
 const SAVE_BUTTON_INIT_VALUE = '保存';
 const SAVE_BUTTON_SAVED_VALUE = 'しました';
 
@@ -24,23 +26,14 @@ const setLastSelectedHostname = (hostname: string): void =>
   localStorage.setItem(LAST_SELECTED_HOST_NAME, hostname);
 
 const App: React.FC = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [textAreaValue, setTextAreaValue] = useState('');
   const [hostname, setHostname] = useState(lastSelectedHostname);
   const [hostnameSet, setHostnameSet] = useState(initHostnameSet);
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const [saveButtonValue, setSaveButtonValue] = useState(
     SAVE_BUTTON_INIT_VALUE
   );
   const [saveButtonTimer, setSaveButtonTimer] = useState<number>();
-
-  const hostnames = Object.keys(hostnameSet);
-  const hostNamesOptions = hostnames.map(hn => {
-    return (
-      <option key={hn} value={hn} selected={hn === hostname}>
-        {hn}
-      </option>
-    );
-  });
 
   useEffect(() => {
     localStorage.setItem(HOSTNAME_SET, JSON.stringify(hostnameSet));
@@ -50,17 +43,22 @@ const App: React.FC = () => {
     setLastSelectedHostname(hostname);
 
     setInputValue(hostname);
+
     if (hostname.length === 0) {
       setTextAreaValue('');
       return;
     }
+
     const style = localStorage.getItem(hostname) || '';
     setTextAreaValue(style);
   }, [hostname]);
 
-  const onInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>): void => {
-      setInputValue(event.target.value);
+  const onSelectChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>): void => {
+      // 選択したoptionタグの値を取ってhostnameとする
+      const selectedOption = event.target.selectedOptions[0];
+      const newHostname = selectedOption.value;
+      setHostname(newHostname);
     },
     []
   );
@@ -68,6 +66,13 @@ const App: React.FC = () => {
   const onTextAreaChange = useCallback(
     (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
       setTextAreaValue(event.target.value);
+    },
+    []
+  );
+
+  const onInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setInputValue(event.target.value);
     },
     []
   );
@@ -93,14 +98,14 @@ const App: React.FC = () => {
     setHostname(newHostname);
   }, [hostnameSet, inputValue, textAreaValue, saveButtonTimer]);
 
-  const onSelectChange = useCallback(
-    (event: React.ChangeEvent<HTMLSelectElement>): void => {
-      const selectedOption = event.target.selectedOptions[0];
-      const newHostname = selectedOption.value;
-      setHostname(newHostname);
-    },
-    []
-  );
+  const hostnames = Object.keys(hostnameSet);
+  const hostNamesOptions = hostnames.map(hn => {
+    return (
+      <option key={hn} value={hn} selected={hn === hostname}>
+        {hn}
+      </option>
+    );
+  });
 
   return (
     <div>
