972aadcdb6a6cf5188d11326d2d129983eecba52
hogashi <hogextend@gmail.com>
2020-12-30 19:53:23 +0900

    fix indent

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
                 '@babel/preset-typescript',
                 [
                   '@babel/preset-react',
-                    // for new jsx transform in react 17 https://babeljs.io/blog/2020/03/16/7.9.0#a-new-jsx-transform-11154httpsgithubcombabelbabelpull11154
+                  // for new jsx transform in react 17 https://babeljs.io/blog/2020/03/16/7.9.0#a-new-jsx-transform-11154httpsgithubcombabelbabelpull11154
                   { runtime: 'automatic' },
                 ],
               ],
