b0ae01415d7ada7d45ec812b0bd0473bffe39602
hogashi <hogextend@gmail.com>
2021-01-07 23:37:38 +0900

    use ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
             options: {
               presets: [
                 '@babel/preset-env',
-                '@babel/preset-typescript',
                 [
                   '@babel/preset-react',
                   // for new jsx transform in react 17 https://babeljs.io/blog/2020/03/16/7.9.0#a-new-jsx-transform-11154httpsgithubcombabelbabelpull11154
@@ -35,6 +34,7 @@ module.exports = {
               ],
             },
           },
+          'ts-loader',
         ],
         exclude: /node_modules/,
       },
