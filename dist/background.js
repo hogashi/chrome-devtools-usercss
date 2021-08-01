d92703fb64bb482a9d371161e0c3bb30522c5a50
hogashi <hogextend@gmail.com>
2021-08-01 17:55:40 +0900

    rebuild

--- dist/background.js
+++ dist/background.js
@@ -13,6 +13,15 @@ __webpack_unused_export__ = ({
 });
 
 var utils_1 = __webpack_require__(6844);
+/**
+ * sendResponseを非同期で呼んでもconnectionを保ってもらうことをtrueを返すことで示す
+ * ref: https://developer.chrome.com/docs/extensions/reference/runtime/#event-onMessage
+ * >This function becomes invalid when the event listener returns, unless
+ * >you return true from the event listener to indicate you wish to send a
+ * >response asynchronously (this will keep the message channel open to the
+ * >other end until sendResponse is called).
+ */
+
 
 var getStyle = function getStyle(message, _, sendResponse) {
   var hostname = message.hostname;
