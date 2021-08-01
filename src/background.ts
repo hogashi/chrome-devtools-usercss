b5469c89afee23434752937efbef53b12057d785
hogashi <hogextend@gmail.com>
2021-08-01 17:33:15 +0900

    add comment for async sendResponse

--- src/background.ts
+++ src/background.ts
@@ -10,6 +10,14 @@ export interface GetStyleMessage {
 
 export type SendGetStyleResponse = (response: { style: string }) => void;
 
+/**
+ * sendResponseを非同期で呼んでもconnectionを保ってもらうことをtrueを返すことで示す
+ * ref: https://developer.chrome.com/docs/extensions/reference/runtime/#event-onMessage
+ * >This function becomes invalid when the event listener returns, unless
+ * >you return true from the event listener to indicate you wish to send a
+ * >response asynchronously (this will keep the message channel open to the
+ * >other end until sendResponse is called).
+ */
 const getStyle = (
   message: GetStyleMessage,
   _: unknown,
